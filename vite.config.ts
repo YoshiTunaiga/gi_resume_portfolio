3cecee525a4fa82b1fa7d84eb07c74186090037d
Gi Diaz <85481752+YoshiTunaiga@users.noreply.github.com>
2025-01-16 18:17:36 -0500

    fix: vite config build option

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,20 @@
-import { resolve } from "path";
 import { defineConfig } from "vite";
+import path from "path";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
+    sourcemap: true,
     rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        nested: resolve(__dirname, "nested/index.html"),
-      },
+      input: "./index.html",
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
     },
   },
 });
