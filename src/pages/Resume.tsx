1c7f4d14d23972ca2303ed125484c92062badd25
Gi Diaz <85481752+YoshiTunaiga@users.noreply.github.com>
2025-01-16 19:28:00 -0500

    fix: fix viteconfig error

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -4,33 +4,6 @@ import ProfessionalExperience from "../components/ResumeComponents/ProfessionalE
 import Education from "../components/ResumeComponents/Education";
 import Volunteering from "../components/ResumeComponents/Volunteering";
 import { TabPanel } from "../components/TabPanel";
-import "../styles/resume.css";
-
-/*
-interface TabPanelProps {
-  children?: React.ReactNode;
-  index: number;
-  value: number;
-}
-
-function TabPanel(props: TabPanelProps) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`tabpanel-${index}`}
-      {...other}>
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
-*/
 
 const Resume = () => {
   const [value, setValue] = useState(0);
